70bdf7394e2b8f8af53bc96cf735c4fa18fe8916
Marcelo Nogueira <marcelon27@gmail.com>
2019-06-18 18:38:40 -0300

    some adjustments in server.js

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const config = require('./webpack.config')
 
+const port = 3000
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
   stats: { colors: true }
-}).listen(3000, (err) => {
+}).listen(port, err => {
   if (err) {
     console.log(err)
   }
 
-  console.log('Listening on http://localhost:3000');
+  console.log(`Listening on http://localhost:${port}`)
 })
