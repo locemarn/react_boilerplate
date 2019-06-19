6f002015e896565702cb615403094c9471b33472
Marcelo Nogueira <marcelon27@gmail.com>
2019-06-19 15:59:42 -0300

    add a jest to tests

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import style from './title.css'
+// import style from './title.css'
 
 const Title = () => {
-  return <h1 className={style.color}>Title</h1>
+  return <h1>Title</h1>
 }
 
 export default Title
